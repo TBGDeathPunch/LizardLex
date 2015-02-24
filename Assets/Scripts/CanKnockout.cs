e5a11cfb161787f2aec9f25a61f3c34b8cacd42d
TBGDeathPunch <sbeatty001@gmail.com>
2015-02-24 17:10:32 -0600

    GuardKnockout added

---
+++ Assets/Scripts/CanKnockout.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanKnockout : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.gameObject.tag == "Player") {
+			other.gameObject.SendMessage("canknockout");
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerKnockout.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerKnockout : MonoBehaviour {
+	public GameObject other;
+	public bool playerBehindGuard = false;
+
+	void canknockout ()
+	{
+			playerBehindGuard = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (playerBehindGuard == true) {
+					if (Input.GetKeyDown (KeyCode.Space)) {
+								Destroy (other);
+				playerBehindGuard = false;
+					}
+			}
+	}
+}
