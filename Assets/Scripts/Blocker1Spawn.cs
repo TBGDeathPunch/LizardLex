fc0d7de66c46f594b80e0415d5f81b158c550e4c
TBGDeathPunch <sbeatty001@gmail.com>
2015-03-31 18:03:28 -0500

    Adding Rest of Guard Sprites and Blockers working

---
+++ Assets/Scripts/Blocker1Spawn.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blocker1Spawn : MonoBehaviour {
+	public GameObject Blocker1;
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.gameObject.tag == "Player") {
+			Blocker1.SendMessage("blocker1place");
+		}
+	}
+}
---
+++ Assets/Scripts/Blocker2Spawn.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blocker2Spawn : MonoBehaviour {
+	public GameObject Blocker2;
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.gameObject.tag == "Player") {
+			Blocker2.SendMessage("blocker2place");
+		}
+	}
+}
