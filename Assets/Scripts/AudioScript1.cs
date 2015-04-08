4793f9f91fe8d5d7148e92e2b5e5522824d491e9
TBGDeathPunch <samuel.beatty@loop.colum.edu>
2015-04-07 19:59:00 -0500

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Library/guidmapper

---
+++ Assets/Scripts/AudioScript1.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioScript1 : MonoBehaviour {
+	AudioSource audioSource;
+	public AudioClip audioClip;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = this.gameObject.AddComponent<AudioSource>();
+		audioSource.PlayOneShot(audioClip);
+	}
+
+}
