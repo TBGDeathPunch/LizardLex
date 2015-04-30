ad531be4f5786b052216990ce5a3b337d836868e
TBGDeathPunch <sbeatty001@gmail.com>
2015-04-30 03:29:13 -0500

    New Blocker and More Sound Work

---
+++ Assets/Scripts/Blocker3Placer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blocker3Placer : MonoBehaviour {
+	public bool canmoveblocker3 = false;
+	
+	public void blocker3place ()
+	{
+		canmoveblocker3 = true;
+	}
+	
+	// moves Main Camera to new position over Level 2
+	void Update () {
+		if (canmoveblocker3 == true) {
+			Vector3 newPosition = transform.position;
+			newPosition.x = 365;
+			newPosition.y = 1050;
+			newPosition.z = 0;
+			transform.position = newPosition;
+		}
+	}
+}
---
+++ Assets/Scripts/Blocker3Spawn.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blocker3Spawn : MonoBehaviour {
+	public GameObject Blocker3;
+	
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.gameObject.tag == "Player") {
+			Blocker3.SendMessage("blocker3place");
+		}
+	}
+}
